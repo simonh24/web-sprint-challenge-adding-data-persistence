a8db0dfbe66d51c2854c3750703e2d276acfb9e2
Simon <simonh72498@gmail.com>
2020-07-17 13:25:17 -0400

    formatted projectsmodel for readability

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -17,7 +17,8 @@ function getProjects() {
 }
 
 function addProject(proj) {
-    return db('projects').insert(proj);
+    return db('projects')
+        .insert(proj);
 }
 
 function getResources() {
@@ -25,25 +26,60 @@ function getResources() {
 }
 
 function addResource(resource) {
-    return db('resources').insert(resource);
+    return db('resources')
+        .insert(resource);
 }
 
 function getTasks() {
-    return db('projects').select('tasks.id', 'projects.projectName', 'projects.description as projectDescription', 'tasks.description as taskDescription', 'tasks.completed as taskCompleted', 'tasks.notes as taskNotes').join('tasks', 'projects.id', '=', 'tasks.projectId');
+    return db('projects')
+        .select('tasks.id',
+            'projects.projectName',
+            'projects.description as projectDescription',
+            'tasks.description as taskDescription',
+            'tasks.completed as taskCompleted',
+            'tasks.notes as taskNotes')
+        .join('tasks', 'projects.id', '=', 'tasks.projectId');
 }
 
 function addTask(task) {
-    return db('tasks').insert(task);
+    return db('tasks')
+        .insert(task);
 }
 
 function getIdTask(id) {
-    return db('projects').select('tasks.id', 'projects.projectName', 'projects.description as projectDescription', 'tasks.description as taskDescription', 'tasks.completed as taskCompleted', 'tasks.notes as taskNotes', 'tasks.projectId as projectId').join('tasks', 'projects.id', '=', 'tasks.projectId').where('projectId', id);
+    return db('projects')
+        .select('tasks.id', 'projects.projectName',
+            'projects.description as projectDescription',
+            'tasks.description as taskDescription',
+            'tasks.completed as taskCompleted',
+            'tasks.notes as taskNotes',
+            'tasks.projectId as projectId')
+        .join('tasks', 'projects.id', '=', 'tasks.projectId')
+        .where('projectId', id);
 }
 
 function getProjectResources() {
-    return db('projectResources').select('projects.id', 'projects.projectName', 'projects.description', 'projects.completed', 'resources.resourceName', 'resources.description as resourceDescription').join('projects', 'projects.id', '=', 'projectResources.projectId').join('resources', 'resources.id', '=', 'projectResources.resourceId');
+    return db('projectResources')
+        .select('projects.id',
+            'projects.projectName',
+            'projects.description',
+            'projects.completed',
+            'resources.resourceName',
+            'resources.description as resourceDescription')
+        .join('projects', 'projects.id', '=', 'projectResources.projectId')
+        .join('resources', 'resources.id', '=', 'projectResources.resourceId');
 }
 
 function getResourceProjects(id) {
-    return db('projectResources').select('projectResources.id as id', 'projectResources.projectId', 'projects.projectName', 'projects.description as projectDescription', 'projects.completed', 'projectResources.resourceId', 'resources.resourceName', 'resources.description as resourceDescription').join('projects', 'projects.id', '=', 'projectResources.projectId').join('resources', 'resources.id', '=', 'projectResources.resourceId').where('resourceId', id);
+    return db('projectResources')
+        .select('projectResources.id as id',
+            'projectResources.projectId',
+            'projects.projectName',
+            'projects.description as projectDescription',
+            'projects.completed', 'projectResources.resourceId',
+            'resources.resourceName',
+            'resources.description as resourceDescription')
+        .join('projects', 'projects.id', '=', 'projectResources.projectId')
+        .join('resources', 'resources.id', '=', 'projectResources.resourceId')
+        .where('resourceId', id);
 }
